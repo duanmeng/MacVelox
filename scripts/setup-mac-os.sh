84b9bc18002f97f9d40ec30dd3f79467302f3c92
macduan <macduan@tencent.com>
2022-10-31 16:14:41 +0800

    set CPU_ARCH and CPU_TARGET in setup script

--- scripts/setup-mac-os.sh
+++ scripts/setup-mac-os.sh
@@ -1,6 +1,55 @@
 #!/bin/bash
 set -eufx -o pipefail
 
+function setup_cpu_target_arch {
+  local OS
+  OS=$(uname)
+  local MACHINE
+  MACHINE=$(uname -m)
+  CPU_ARCH=""
+  CPU_TARGET=$CPU_ARCH
+
+  echo "defult CPU_ARCH=$CPU_ARCH"
+  echo "defult CPU_TARGET=$CPU_TARGET"
+
+  if [ -z "$CPU_ARCH" ]; then
+    if [ "$OS" = "Darwin" ]; then
+      if [ "$MACHINE" = "x86_64" ]; then
+        local CPU_CAPABILITIES
+        CPU_CAPABILITIES=$(sysctl -a | grep machdep.cpu.features | awk '{print tolower($0)}')
+
+        if [[ $CPU_CAPABILITIES =~ "avx" ]]; then
+          CPU_ARCH="avx"
+        else
+          CPU_ARCH="sse"
+        fi
+
+      elif [[ $(sysctl -a | grep machdep.cpu.brand_string) =~ "Apple" ]]; then
+        # Apple silicon.
+        CPU_ARCH="arm64"
+      fi
+
+    else [ "$OS" = "Linux" ];
+      local CPU_CAPABILITIES
+      CPU_CAPABILITIES=$(cat /proc/cpuinfo | grep flags | head -n 1| awk '{print tolower($0)}')
+
+      if [[ "$CPU_CAPABILITIES" =~ "avx" ]]; then
+            CPU_ARCH="avx"
+      elif [[ "$CPU_CAPABILITIES" =~ "sse" ]]; then
+            CPU_ARCH="sse"
+      elif [ "$MACHINE" = "aarch64" ]; then
+            CPU_ARCH="aarch64"
+      fi
+    fi
+  fi
+  CPU_TARGET=$CPU_ARCH
+
+  echo "set CPU_ARCH=$CPU_ARCH"
+  echo "set CPU_TARGET=$CPU_TARGET"
+}
+
+setup_cpu_target_arch
+
 git submodule sync --recursive
 git submodule update --init --recursive
 
